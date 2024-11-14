de712d34148f8dbd58ed85e7de36d7f637634d86
LanceMacNal1 <166206937+LanceMacNal1@users.noreply.github.com>
2024-11-15 07:45:28 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -179,6 +179,21 @@ elif st.session_state.page_selection == "eda":
         
     with col[1]:
         st.markdown('#### Graphs Column 2')
+        prodsales_pd = pd.DataFrame({
+            'MntWines': [clean_pd['MntWines'].sum()],
+            'MntFruits': [clean_pd['MntFruits'].sum()],
+            'MntMeatProducts': [clean_pd['MntMeatProducts'].sum()],
+            'MntFishProducts': [clean_pd['MntFishProducts'].sum()],
+            'MntSweetProducts': [clean_pd['MntSweetProducts'].sum()],
+            'MntGoldProds': [clean_pd['MntGoldProds'].sum()]
+        })
+
+        prodsales_pivot = prodsales_pd.melt(var_name="Product", value_name="TotalSales")
+        st.write(prodsales_pivot)
+
+        plt.figure(figsize=(10, 6))
+        sns.barplot(x='Product', y='TotalSales', data=prodsales_pivot, palette='viridis')
+        st.pyplot()
        
     with col[2]:
         st.markdown('#### Graphs Column 3')
